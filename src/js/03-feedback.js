4d0c66b2cb05d17078fa686b312eca5545530d66
Prysmytskyi-Oleksandr <san2003@ukr.net>
2022-11-20 19:22:19 +0200

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,34 @@
-
+import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', onTextareaInput);
+form.addEventListener('input', throttle(onTextareaInput, 500));
 form.addEventListener('submit', onFormSubmit);
 window.addEventListener('load', getFromLocalStorage);
 
 function onTextareaInput () {
     const email = form.elements.email.value;
-    const message = form.elements.message.value;
-    // console.log(email);
-    // console.log(textarea);
+    const message = form.elements.message.value;    
     localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+   
     event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+
 };
  
 function getFromLocalStorage() {
     const saveInformations = localStorage.getItem('feedback-form-state');
-    console.log(saveInformations);
-    
+       
     if (saveInformations) {
         const parsSaveInformations = JSON.parse(saveInformations);
-
-        console.log(parsSaveInformations);
-        
-        form.elements.email.value = parsSaveInformations.email;
+         
+       form.elements.email.value = parsSaveInformations.email;
         form.elements.message.value = parsSaveInformations.message;
-}
+    }
 }
 
